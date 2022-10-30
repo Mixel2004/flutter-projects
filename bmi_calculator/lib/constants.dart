0cfa2d9fc5b132e8d7e426e3e6f5315efd2ab586
Mixel2004 <84668201+Mixel2004@users.noreply.github.com>
2022-10-30 22:39:56 +0530

    Create constants.dart

---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const kActiveContainerColor = Color(0xFF1D1E33);
+const kInactiveContainerColor = Color(0xFF111328);
+const kButtonContainerColor = Color(0xFFEB1555);
+
+const TextStyle kTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8D8E98),
+);
+const TextStyle kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+const TextStyle kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
